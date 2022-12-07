cc0266f8b0ab6638b06bfb8d4061e6d005214885
Daniel Ramos <danielramosacosta1@gmail.com>
2022-12-07 01:54:28 +0000

    update boilerplate

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,4 +1,5 @@
-import { myFunction } from "./main"
+import { describe, it, expect } from "vitest"
+import { myFunction } from "./main.js"
 
 describe("Default test", () => {
   it("should work", () => {
