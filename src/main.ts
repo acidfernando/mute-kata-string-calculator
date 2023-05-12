17f1c7365036da592177b08226975b46a25086dd
Alberto González <albertogonzalez@acidtango.com>
2023-05-12 17:30:37 +0100

    work with custom delimiters

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 export function add(numbers: string) {
   if (!numbers) return 0
 
-  const listOfNumbers = numbers.replace(/\n/g, ",").split(",")
+  let delimiter = ","
+  let finalNumbers = numbers
+
+  if (numbers.slice(0, 2) === "//") {
+    delimiter = numbers[2]
+    finalNumbers = numbers.slice(4)
+  }
+  const listOfNumbers = finalNumbers.replace(/\n/g, delimiter).split(delimiter)
   const result = listOfNumbers.reduce((acc, current) => parseInt(current) + acc, 0)
 
   return result
