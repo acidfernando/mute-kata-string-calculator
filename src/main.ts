7fc001b8655e68565e5416d9fdb00460de6f14b8
Fernando García Hernández <86410308+acidfernando@users.noreply.github.com>
2023-05-12 17:21:57 +0100

    fix: broken test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 export function add(numbers: string) {
   if (!numbers) return 0
 
-  const listOfNumbers = numbers.split(",")
+  const listOfNumbers = numbers.replace(/\n/g, ",").split(",")
   const result = listOfNumbers.reduce((acc, current) => parseInt(current) + acc, 0)
 
   return result
