93b089810bdfb7ddc48c749f576cc4a1f5290a18
Fernando García Hernández <86410308+acidfernando@users.noreply.github.com>
2023-05-12 17:13:34 +0100

    fix: test name

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest"
 import { add } from "./main.js"
 
 describe("Default test", () => {
-  it("shouldn't do anything if numbers not passed", () => {
-    expect(add("")).toEqual(undefined)
+  it("should return 0 if no input is given", () => {
+    expect(add("")).toEqual(0)
   })
 })
